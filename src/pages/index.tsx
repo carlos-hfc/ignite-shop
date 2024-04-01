48271e7a86774f9766e1ad388824895695d41a8d
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-01 12:08:24 -0300

    feat: add keen slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
+import "keen-slider/keen-slider.min.css"
+
+import { useKeenSlider } from "keen-slider/react"
 import Image from "next/image"
 
 import shirt1 from "../assets/shirts/1.png"
+import shirt2 from "../assets/shirts/2.png"
+import shirt3 from "../assets/shirts/3.png"
 import { HomeContainer, Product } from "../styles/pages/home"
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer
+      ref={sliderRef}
+      className="keen-slider"
+    >
+      <Product className="keen-slider__slide">
         <Image
           src={shirt1}
           width={520}
@@ -20,9 +35,23 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image
-          src={shirt1}
+          src={shirt3}
+          width={520}
+          height={480}
+          alt=""
+        />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image
+          src={shirt2}
           width={520}
           height={480}
           alt=""
