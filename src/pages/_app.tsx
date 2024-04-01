9e695967680b008107d7dafda038538da1610a45
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-01 11:05:33 -0300

    feat: add header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import { AppProps } from "next/app"
 
+import logo from "../assets/logo.svg"
 import { globalStyles } from "../styles/global"
+import { Container, Header } from "../styles/pages/app"
 
 globalStyles()
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img
+          src={logo.src}
+          alt=""
+        />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
